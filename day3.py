c7f8fa0c98b3c5ed53ce77dad9381a73c7448911
Johan <johanhuu@gmail.com>
2025-12-03 21:04:53 +0100

    refactor, part2 still scuffed

--- day3.py
+++ day3.py
@@ -5,48 +5,32 @@ def parse_input() -> list[list[int]]:
 
 
 def part1(data: list[list[int]]) -> int:
-    ans = 0
+    answer = 0
     for battery in data:
-        largest = "0"
-        largest_index = battery[:-1].index(max(battery[:-1]))
-        
-        if largest_index == len(battery) - 2:
-            largest = str(battery[largest_index]) + str(battery[-1])
-        else:
-            second_slice = battery[largest_index + 1:]
-            second_largest = battery[battery.index(max(second_slice), largest_index + 1)]
-            largest = str(battery[largest_index]) + str(second_largest)
+        largest_idx, value = max(enumerate(battery[:-1]), key=lambda x: x[1])
+        second_slice = battery[largest_idx + 1:]
+        value2 = max(second_slice)
+        answer += int(str(value) + str(value2))
 
-        ans += int(largest)
-
-    return ans
+    return answer
 
 
 def part2(data: list[list[int]]) -> int:
-    ans = 0
+    answer = 0
     for battery in data:
-        largest = "0"
-        
-        first_index = battery.index(max(battery[:-11]))
-        
-        if first_index == len(battery) - 12:
-            rest = "".join(map(str, battery[first_index + 1:]))
-            largest = str(battery[first_index]) + rest
-        else:
-            next_index = first_index + 1
-            items = []
-            for n in range(10, -1, -1):
-                next_slice = battery[next_index:-n] if n > 0 else battery[next_index:]
-                next_max = battery.index(max(next_slice), next_index)
-                next_value = str(battery[next_max])
-                items.append(next_value)
+        first_idx, value = max(enumerate(battery[:-11]), key=lambda x: x[1])
+        next_idx = first_idx + 1
+        items = []
 
-                next_index = next_max + 1
-        
-            largest = str(battery[first_index]) + "".join(items)
-        ans += int(largest)
+        for n in range(10, -1, -1):
+            next_slice = battery[next_idx:-n] if n > 0 else battery[next_idx:]
+            next_max_idx = battery.index(max(next_slice), next_idx)
+            items.append(str(battery[next_max_idx]))
+            next_idx = next_max_idx + 1
+    
+        answer += int(str(value) + "".join(items))
 
-    return ans
+    return answer
 
 
 def main() -> None:
