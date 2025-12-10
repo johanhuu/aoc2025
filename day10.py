074b0cdc014107be27a5231a91546cb65c8124a8
Johan <johanhuu@gmail.com>
2025-12-10 18:59:14 +0100

    z3 xddd

--- day10.py
+++ day10.py
@@ -1,35 +1,37 @@
-from functools import cache
 from typing import Deque
+import z3  # type: ignore
+
 
 def parse_input():
     lights = []
     buttons = []
     joltage = []
-    #with open("files/input10.txt") as f:
-    with open("files/example10.txt") as f:
+    with open("files/input10.txt") as f:
+        # with open("files/example10.txt") as f:
         for line in f.readlines():
-            line = line.rstrip()
-            res = line.split(" ")
-            lights.append(res[0].lstrip("[").rstrip("]"))
+            segments = line.rstrip().split()
+            lights.append(segments[0].lstrip("[").rstrip("]"))
             tmp = []
-            for button in res[1:-1]:
+            for button in segments[1:-1]:
                 button = button.lstrip("(").rstrip(")")
                 tmp.append(tuple(map(int, button.split(","))))
             buttons.append(tuple(tmp))
-            joltage.append(tuple(map(int, res[-1].lstrip("{").rstrip("}").split(","))))
+            joltage.append(
+                tuple(map(int, segments[-1].lstrip("{").rstrip("}").split(",")))
+            )
 
     return lights, buttons, joltage
 
 
 def part1(data) -> int:
     answer = 0
-    lights, buttons, _  = data
+    lights, buttons, _ = data
 
     for i, light in enumerate(lights):
         current = [False] * len(light)
         target = [s == "#" for s in light]
 
-        queue = Deque()
+        queue = Deque()  # type: ignore
         queue.append((current, 0))
 
         while queue:
@@ -46,42 +48,52 @@ def part1(data) -> int:
 
     return answer
 
-@cache
-def solve(buttons, current, target, steps):
-    ways = 100_000_000
 
-    if current == target:
-        return steps
-    
-    for button in buttons:
-        local_current = list(current)
-        for index in button:
-            local_current[index] += 1
-        if any(local_current[i] > target[i] for i in range(len(current))):
-            continue
-        res = solve(buttons, tuple(local_current), target, steps + 1)
-        ways = min(ways, res)
-
-    return ways
-
+# rip dp rip dijkstra is this really how it's meant to be solved???
 def part2(data) -> int:
     answer = 0
-    _, buttons, joltage  = data
+    _, buttons, joltage = data
 
     for i, button_options in enumerate(buttons):
-        print(i)
         target = joltage[i]
-        current = (0,) * len(target)
+        num_cols = len(target)
 
-        res = solve(tuple(sorted(button_options, key=len)), current, target, 0)
-        answer += res
+        buttons = []
+        for button in button_options:
+            b = [0] * num_cols
+            for index in button:
+                b[index] += 1
+            buttons.append(b)
+
+        num_buttons = len(buttons)
+        variables = [z3.Int(str(i)) for i in range(num_buttons)]  # type: ignore  # noqa: F405
+
+        opt = z3.Optimize()  # type: ignore  # noqa: F405
+
+        for var in variables:
+            opt.add(var >= 0)
+
+        for col in range(num_cols):
+            opt.add(
+                sum(variables[i] * buttons[i][col] for i in range(num_buttons))
+                == target[col]
+            )
+
+        opt.minimize(sum(variables))
+        if opt.check() != z3.sat:  # type: ignore  # noqa: F405
+            print("Solution not found")
+            exit(-1)
+
+        m = opt.model()
+        ans = sum(m[var].as_long() for var in variables)  # type: ignore  # noqa: F405
+        answer += ans
 
     return answer
 
 
 def main() -> None:
     data = parse_input()
-    #print(f"Part 1 answer: {part1(data)}")
+    print(f"Part 1 answer: {part1(data)}")
     print(f"Part 2 answer: {part2(data)}")
 
 
